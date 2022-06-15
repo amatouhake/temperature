0086b9096ce648e20ebd239f81d9faee305efd33
amatouhake <60422818+amatouhake@users.noreply.github.com>
2022-06-15 14:33:25 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,9 @@
+bluetooth.startTemperatureService();
+
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes);
+});
+
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No);
+});
